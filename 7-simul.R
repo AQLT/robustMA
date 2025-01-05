11d3c917de356b650854e7a0e7367b1d002c5aef
AQLT <alain.quartier@yahoo.fr>
2025-01-05 20:19:55 +0100

    degree 2 simul

--- 7-simul.R
+++ 7-simul.R
@@ -10,13 +10,25 @@ niveau_choc <- 0.1
 robust_filters <- readRDS("data/robust_filters.rds")
 simul_ao_td0 <- simul_ls_td0 <- ts(1, start =2020, end = c(2024, 12), frequency = 12)
 date_out <- 2022
+
 window(simul_ao_td0, start = date_out, end = date_out) <-
 	window(simul_ao_td0, start = date_out, end = date_out) * (1 + niveau_choc)
 window(simul_ls_td0, start = date_out) <-
 	window(simul_ls_td0, start = date_out) * (1 + niveau_choc)
 
 simul_ao_td1 <- simul_ao_td0 + (time(simul_ao_td0) - 2020)/frequency(simul_ao_td0)
- simul_ls_td1 <- simul_ls_td0 + (time(simul_ls_td0) - 2020)/frequency(simul_ls_td0)
+simul_ls_td1 <- simul_ls_td0 + (time(simul_ls_td0) - 2020)/frequency(simul_ls_td0)
+
+simul_ao_td2 <- -(time(simul_ao_td0) - date_out)^2
+simul_ls_td2 <- simul_ao_td2 <- simul_ao_td2 + abs(min(simul_ao_td2)) + 1
+window(simul_ao_td2, start = date_out, end = date_out) <-
+	window(simul_ao_td2, start = date_out, end = date_out) * (1 + niveau_choc)
+window(simul_ls_td2, start = date_out) <-
+	window(simul_ls_td2, start = date_out) * (1 + niveau_choc)
+
+################
+### degree 0 ###
+################
 
 res <- lapply(
 	create_vintage(simul_ao_td0, date_out),
@@ -39,6 +51,10 @@ saveRDS(list(data = res,
 )
 
 
+################
+### degree 1 ###
+################
+
 res <- lapply(
 	create_vintage(simul_ao_td1, date_out),
 	all_filtered, 13, lc_f, robust_filters[["ao"]], date_out)
@@ -58,23 +74,48 @@ res <- list(data = res,
 saveRDS(res,
 		file.path(dir_exports, "simul_ls_td1.RDS")
 )
-simul_ao_t2 <- simul_ao_td1 +0.1((time(simul_ao_td0) - 2020))^2
+
+################
+### degree 2 ###
+################
 
 res <- lapply(
-	create_vintage(simul_ao_t2, date_out),
+	create_vintage(simul_ao_td2, date_out),
 	all_filtered, 13, lc_f, robust_filters[["ao"]], date_out)
 res <- list(data = res,
 			out = date_out,
-			y = simul_ao_td0)
-plot_y(res)
-y_as_plot <- FALSE
-add_y <- FALSE
-graph_est <- get_all_plots(res, y_as_plot = y_as_plot, add_y = add_y)
-graph_est$CLF <- NULL
-plot_y(res)
-patchwork::wrap_plots(graph_est, ncol = 3)
-graph_ci <- plot_confint(
-	res,
-	default_filter = lc_f,
-	robust_f = robust_ff[["ao"]])
-patchwork::wrap_plots(graph_ci, ncol = 3)
+			y = simul_ao_td2)
+saveRDS(res,
+		file.path(dir_exports, "simul_ao_td2.RDS")
+)
+
+res <- lapply(
+	create_vintage(simul_ls_td2, date_out),
+	all_filtered, 13, lc_f, robust_filters[["ls"]], date_out)
+res <- list(data = res,
+			out = date_out,
+			y = simul_ls_td2)
+saveRDS(res,
+		file.path(dir_exports, "simul_ls_td2.RDS")
+)
+
+# simul_ao_t2 <- simul_ao_td1 +0.1((time(simul_ao_td0) - 2020))^2
+#
+# res <- lapply(
+# 	create_vintage(simul_ao_t2, date_out),
+# 	all_filtered, 13, lc_f, robust_filters[["ao"]], date_out)
+# res <- list(data = res,
+# 			out = date_out,
+# 			y = simul_ao_td0)
+# plot_y(res)
+# y_as_plot <- FALSE
+# add_y <- FALSE
+# graph_est <- get_all_plots(res, y_as_plot = y_as_plot, add_y = add_y)
+# graph_est$CLF <- NULL
+# plot_y(res)
+# patchwork::wrap_plots(graph_est, ncol = 3)
+# graph_ci <- plot_confint(
+# 	res,
+# 	default_filter = lc_f,
+# 	robust_f = robust_ff[["ao"]])
+# patchwork::wrap_plots(graph_ci, ncol = 3)
